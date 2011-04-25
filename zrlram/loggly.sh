2e950809670be3f558cec29f469c833c4ff43ba7
Jordan Sissel <jls@semicomplete.com>
2011-04-25 15:52:16 -0700

    - remove zrlram

--- zrlram/loggly.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh 
-#tail -f /var/log/loggly/exception /var/log/loggly/loggly /var/log/loggly/cron |  perl -pe 's/ERROR|CRIT/\e[1;31;43m$&\e[0m/g'
-tail -f /var/log/loggly/exception /var/log/loggly/loggly /var/log/loggly/cron 
