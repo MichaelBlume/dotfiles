0e6380fea147fa6792428455ff39a6b1d4dcae16
Mike Blume <mike@loggly.com>
2012-05-16 21:04:32 +0000

    rewrite with envoy

--- git-export.py
+++ git-export.py
@@ -2,18 +2,19 @@
 
 import sys
 
-from commands import getoutput
 from os import listdir
-from subprocess import call
 from time import sleep
+import envoy
 
 BRANCH_MAP = {
   'default': 'master'
 }
 
-def run(*args):
-    retcode = call(args)
-    if retcode:
+def run(cmd):
+    print cmd
+    r = envoy.run(cmd)
+    print r.std_out
+    if r.status_code:
         sys.exit(1)
 
 class GitExporter(object):
@@ -24,13 +25,13 @@ class GitExporter(object):
     def main(self):
         """Update hg, export to git and push up references."""
 
-        run('hg', 'pull', '-u')
-        run('hg', 'gexport')
+        run('hg pull -u')
+        run('hg gexport')
 
         hg_branches = {}
-        hg_heads = getoutput('hg branches --debug --active 2> /dev/null')
+        hg_heads = envoy.run('hg branches --debug --active').std_out
 
-        for line in hg_heads.split('\n'):
+        for line in hg_heads.strip().split('\n'):
             branch, full_rev = line.split()
             branch = BRANCH_MAP.get(branch, branch)
             _local, rev = full_rev.split(':')
@@ -61,13 +62,13 @@ class GitExporter(object):
         if remove:
             for branch in remove:
                 print "# Removing Branch:", branch
-                run('git', 'push', 'mirror', ':' + branch)
+                run('git push mirror :' + branch)
                 del self.git_branches[branch]
 
         print "# Updating Branches"
         for branch in add:
             ref = self.git_branches[branch]
-            run('git', 'push', '-f', 'mirror', "%s:refs/heads/%s" % (ref, branch))
+            run('git push -f mirror %s:refs/heads/%s' % (ref, branch))
 
     def runloop(self):
         while 1:
