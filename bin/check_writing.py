c0ddfda1bbc10605ca646d39f5d1c9695f89795c
Mike Blume <mike@loggly.com>
2012-09-06 11:59:23 -0700

    use beeminder api

--- bin/check_writing.py
+++ bin/check_writing.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python
 import envoy
+import requests
+import time
 
 r = envoy.run("find /home/mike/Dropbox/writing | xargs wc --bytes | tail -n1")
 nothing, count, total = r.std_out.split(' ')
-print int(count) // 1024
+kb_written = int(count) // 1024
+print kb_written
 
+url = "https://www.beeminder.com/api/v1/users/mblume/goals/writing/datapoints.json"
+r = requests.post(url,
+        {'auth_token':'qiqScsHpQmRrSMKDsM1e'
+        ,'timestamp':time.time()
+        ,'value': kb_written
+        })
+print r.text
