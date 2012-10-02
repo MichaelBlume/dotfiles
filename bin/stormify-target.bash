01d94c3421aa636fe6302a9c76ef7b74b2ecf8fb
Mike Blume <mike@loggly.com>
2012-10-02 12:33:13 -0700

    improve stormify
    set up processes

--- bin/stormify-target.bash
+++ bin/stormify-target.bash
@@ -1,3 +1,4 @@
+exec 1>/tmp/stormify-log
 sudo apt-get update
 sudo apt-get install -y openjdk-6-jdk build-essential libtool autoconf automake git uuid-dev pkg-config unzip
 mkdir workspace
@@ -18,5 +19,34 @@ sudo make install
 cd ..
 wget https://github.com/downloads/nathanmarz/storm/storm-0.8.1.zip
 unzip storm-0.8.1.zip
+if [ $ZOOKEEPER ]
+  then
+
+  wget http://apache.petsads.us/incubator/kafka/kafka-0.7.1-incubating/kafka-0.7.1-incubating-src.tgz
+  tar xvfz kafka-0.7.1-incubating-src.tgz
+
+  cd kafka-0.7.1-incubating
+  ./sbt update
+  ./sbt package
+
+  echo "starting zookeeper process"
+  screen -d -m -S zookeeper bin/zookeeper-server-start.sh config/zookeeper.properties
+  cd ..
+fi
+
 cd storm-0.8.1
 cp $HOME/storm.yaml conf/storm.yaml
+if [ $NIMBUS ]
+  then
+  echo "starting nimbus processes"
+  screen -d -m -S nimbus bash -c "while true; do bin/storm nimbus; done"
+  screen -d -m -S ui bash -c "while true; do bin/storm ui; done"
+fi
+
+if [ $SUPERVISOR ]
+  then
+  echo "starting supervisor process"
+  screen -d -m -S supervisor bash -c "while true; do bin/storm supervisor; done"
+fi
+
+echo "finished stormifying box"
--- bin/stormify.py
+++ bin/stormify.py
@@ -1,17 +1,7 @@
 import os
 import pwd
-from threading import Thread
 
 import envoy
-
-run_zookeeper = ';'.join("""
-wget http://apache.petsads.us/incubator/kafka/kafka-0.7.1-incubating/kafka-0.7.1-incubating-src.tgz
-tar xvfz kafka-0.7.1-incubating-src.tgz
-cd kafka-0.7.1-incubating
-./sbt update
-./sbt package
-screen -d -m "bin/zookeeper-server-start.sh config/zookeeper.properties"
-""".split('\n'))
 
 def setup_storm_cluster(hosts, ext_zookeeper_host=None, user=None):
     if user is None:
@@ -30,36 +20,33 @@ storm.local.dir: /home/%s/storm
     with open("storm_conf.tmp", "w") as f:
         f.write(storm_config)
 
-    target_script = os.path.join(os.path.dirname(__file__), 'stormify-target')
+    target_script = os.path.join(os.path.dirname(__file__),
+            'stormify-target.bash')
 
     run_zk = ext_zookeeper_host is None
-    threads = [Thread(target=stormify_host,
-                      args=(host, n, user, target_script, run_zk))
-               for n, host in enumerate(hosts)]
-    for t in threads: t.run()
-    for t in threads: t.join()
-
-def do_forever(full_host, storm_command):
-    envoy.run("""ssh %s 'cd workspace/storm-0.8.1/;
-            screen -d -m "while true; do bin/storm %s; done"' """ %
-            (full_host, storm_command))
+    for n, host in enumerate(hosts):
+        stormify_host(host, n, user, target_script, run_zk)
 
 def stormify_host(host, number, user, target_script, run_zk):
     full_host = "%s@%s" % (user, host)
-    print 'running scp'
-    envoy.run("scp %s %s:" % (target_script, full_host))
-    print 'scp done! running stormify script'
-    envoy.run('ssh %s "bash stormify-target > stormsetuplog"' % full_host)
-    print 'moving config file into place'
-    envoy.run("scp storm_conf.tmp %s:workspace/storm-0.8.1/config/storm.yaml" %
-            full_host)
-    print 'setting up daemons'
+
+    flags = ''
     if number == 0:
         # we're setting up a nimbus
         if run_zk:
-            envoy.run(" ssh %s '%s'" % (full_host, run_zookeeper))
+            flags = 'ZOOKEEPER=1 ' + flags
 
-        do_forever(full_host, 'nimbus')
-        do_forever(full_host, 'ui')
+        flags = 'NIMBUS=1 ' + flags
     else:
-        do_forever(full_host, 'supervisor')
+        flags = 'SUPERVISOR=1 ' + flags
+
+    print 'scp-ing stormify script'
+    envoy.run("scp %s %s:" % (target_script, full_host))
+    print 'scp-ing storm config'
+    envoy.run("scp storm_conf.tmp %s:storm.yaml" %
+            full_host)
+    print 'scp done! running stormify script'
+    cmd = ('''ssh %s "%sscreen -d -m bash stormify-target.bash"'''
+            % (full_host, flags))
+    print cmd
+    envoy.run(cmd)
