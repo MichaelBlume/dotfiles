f04cc4a7e946039fcc57cd88e20b76f99f033b19
Mike Blume <mike@loggly.com>
2013-07-07 15:56:46 -0700

    add lein-pprint

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -2,5 +2,6 @@
                   [lein-cloverage "1.0.2"]
                   [lein-kibit "0.0.8"]
                   [lein-swank "1.4.5"]
+                  [lein-pprint "1.1.1"]
                   [lein-clojars "0.9.1"]]
         :aliases {"ir" ["trampoline" "irepl"]}}}
