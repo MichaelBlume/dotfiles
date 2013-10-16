b76dae7d23b9a22e90a3384984b9b47e12a95b40
Mike Blume <mike@loggly.com>
2013-10-16 11:05:05 -0700

    upgrade a couple clojure plugins

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,8 +1,8 @@
-{:user {:plugins [[lein-iclojure "1.2-SNAPSHOT"]
+{:user {:plugins [[lein-iclojure "1.2"]
                   [lein-cloverage "1.0.2"]
                   [lein-kibit "0.0.8"]
                   [lein-swank "1.4.5"]
                   [lein-pprint "1.1.1"]
-                  [lein-try "0.2.0"]
+                  [lein-try "0.3.2"]
                   [lein-clojars "0.9.1"]]
         :aliases {"ir" ["trampoline" "irepl"]}}}
