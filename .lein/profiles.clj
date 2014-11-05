c6723619423d3e5e4011c01338b3f79eea2da5d6
Mike Blume <michael.blume@climate.com>
2014-11-05 12:58:16 -0800

    cute humane test output thing

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -6,7 +6,10 @@
                   [lein-ancient "0.5.5"]
                   [jonase/eastwood "0.1.4"]
                   [lein-clojars "0.9.1"]]
+        :injections  [(require 'pjstadig.humane-test-output)
+                      (pjstadig.humane-test-output/activate!)]
         :dependencies [[slamhound "1.5.5"]
+                       [pjstadig/humane-test-output "0.6.0"]
                        [mike-clj "1.0.0-SNAPSHOT"]
                        [com.cemerick/pomegranate "0.3.0"]
                        [criterium "0.4.3"]]}}
